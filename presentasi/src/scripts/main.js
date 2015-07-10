cc91fff84bc674a8981223ffda329649e68d3b5c
RandyMuhroji <131111661@students.mikroskil.ac.id>
2015-07-10 15:17:53 +0700

    tes aja

--- presentasi/src/scripts/main.js
+++ presentasi/src/scripts/main.js
@@ -52,3 +52,19 @@ bespoke.from('article', [
 require('prism');
 require('bespoke-spotlight');
 
+$(function() {
+  var body = $('body');
+  var backgrounds = ['url(http://static.jsbin.com/images/jsbin_static.png)', 'url(http://static.jsbin.com/images/popout.png)'];
+  var current = 0;
+
+  function nextBackground() {
+    body.css(
+    'background',
+      backgrounds[current = ++current % backgrounds.length]
+    );
+
+   setTimeout(nextBackground, 5000);
+   }
+   setTimeout(nextBackground, 5000);
+     body.css('background', backgrounds[0]);
+   });
